a8a0b2d658a9391f81596369c141d965975bca38
krashmello <joelenrikesoteldo@gmail.com>
2025-03-27 12:05:28 -0400

    feat: addapter swagger prisma and structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('km warehouse docs')
+    .setDescription('una api sencilla')
+    .setVersion('1.0')
+    .addTag('default')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
