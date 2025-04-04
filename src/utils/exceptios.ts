c211c94837f59d01d94d7514655d9bf754a12bf5
krashmello <joelenrikesoteldo@gmail.com>
2025-04-04 06:46:03 -0400

    feat: exceptions

--- src/utils/exceptios.ts
+++ src/utils/exceptios.ts
@@ -4,32 +4,39 @@ interface opt {
   type?: 'WARNING' | 'SUCCESS' | 'DANGER' | 'INFO'
 }
 
-export const NotFound = (opt: opt) => {
-  const { data, type } = opt
-  const status = HttpStatus.NOT_FOUND
-  throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+export default (opt: {
+  data: string | Record<string, any>
+  type?: 'WARNING' | 'SUCCESS' | 'DANGER' | 'INFO'
+  status: HttpStatus
+}) => {
+  const { status, data, type } = opt
+  return response[status]({ data, type })
 }
 
-export const BadRequest = (opt: opt) => {
-  const { data, type } = opt
-  const status = HttpStatus.BAD_REQUEST
-  throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
-}
-
-export const Create = (opt: opt) => {
-  const { data, type } = opt
-  const status = HttpStatus.CREATED
-  throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
-}
-
-export const Ok = (opt: opt) => {
-  const { data, type } = opt
-  const status = HttpStatus.OK
-  throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
-}
-
-export const Unauthorized = (opt: opt) => {
-  const { data, type } = opt
-  const status = HttpStatus.UNAUTHORIZED
-  throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+const response = {
+  404: (opt: opt) => {
+    const { data, type } = opt
+    const status = HttpStatus.NOT_FOUND
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
+  400: (opt: opt) => {
+    const { data, type } = opt
+    const status = HttpStatus.BAD_REQUEST
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
+  201: (opt: opt) => {
+    const { data, type } = opt
+    const status = HttpStatus.CREATED
+    throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
+  },
+  200: (opt: opt) => {
+    const { data, type } = opt
+    const status = HttpStatus.OK
+    throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
+  },
+  401: (opt: opt) => {
+    const { data, type } = opt
+    const status = HttpStatus.UNAUTHORIZED
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
 }
