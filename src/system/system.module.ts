4f771dd5c5684aadb907135f764aae123c3e4568
krashmello <joelenrikesoteldo@gmail.com>
2025-04-06 11:33:01 -0400

    feat: update system module
    adapter guards

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
+import { APP_GUARD } from '@nestjs/core'
+import { RouteGuard } from 'src/guard/route.guard'
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RouteGuard,
+    },
+  ],
 })
 export class SystemModule {}
