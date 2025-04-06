dd12fdf17727b9fc869a3b2c98bd38c708b68664
krashmello <joelenrikesoteldo@gmail.com>
2025-04-06 11:30:01 -0400

    feat: update app versions

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
 
   const config = new DocumentBuilder()
     .setTitle('km warehouse docs')
     .setDescription('una api sencilla')
     .setVersion('1.0')
     .addTag('default')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, documentFactory);
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, documentFactory)
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT ?? 5000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  await app.listen(process.env.PORT ?? 5000)
 }
-bootstrap();
+bootstrap()
