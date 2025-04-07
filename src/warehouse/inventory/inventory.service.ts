564adcd7aeeb07bd27d78433f867ebc07e5b4706
krashmello <joelenrikesoteldo@gmail.com>
2025-04-07 06:13:23 -0400

    feat: update inventory service
    add the functions count and find all but no have a real functionality

--- src/warehouse/inventory/inventory.service.ts
+++ src/warehouse/inventory/inventory.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common'
+import { TryCatch } from 'src/decorators/core.decoratos'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class InventoryService {}
+export class InventoryService {
+  constructor(private prisma: PrismaService) {}
+
+  @TryCatch()
+  private async count() {
+    return await this.prisma.inventory.count()
+  }
+
+  @TryCatch()
+  async findAll(opt: { search: string; limit?: number; page?: number }) {
+    const { search, limit, page } = opt
+    return await this.prisma.inventory.findMany({
+      where: {
+        name: {
+          contains: search,
+        },
+      },
+      take: limit,
+      skip: page,
+    })
+  }
+}
