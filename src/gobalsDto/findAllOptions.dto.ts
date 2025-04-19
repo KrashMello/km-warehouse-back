4d6ed36a32eec6307ac1a69ecf1aa6985e57a4a8
krashmello <joelenrikesoteldo@gmail.com>
2025-04-18 20:51:53 -0400

    feat: update globals dto

--- src/gobalsDto/findAllOptions.dto.ts
+++ src/gobalsDto/findAllOptions.dto.ts
@@ -1,25 +1,28 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNumberString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNumberString, IsOptional } from 'class-validator'
 
 export class FindAllOptionsDto {
   @ApiProperty({
     name: 'search',
+    required: false,
     example: '',
   })
   @IsOptional()
-  search?: string;
+  search?: string
   @ApiProperty({
     name: 'page',
+    required: false,
     example: 1,
   })
   @IsOptional()
   @IsNumberString()
-  page?: number;
+  page?: number
   @ApiProperty({
     name: 'limit',
+    required: false,
     example: 10,
   })
   @IsOptional()
   @IsNumberString()
-  limit?: number;
+  limit?: number
 }
