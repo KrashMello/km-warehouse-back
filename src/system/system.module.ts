ce9205ff5a76033485bc7b0805d8a18d8a99da6d
KrashMello <joelenrikesoteldo@gmail.com>
2025-05-04 11:41:37 -0400

    feat: update system module

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -3,9 +3,11 @@ import { UserModule } from './user/user.module'
 import { AuthModule } from './auth/auth.module'
 import { APP_GUARD } from '@nestjs/core'
 import { RouteGuard } from 'src/guard/route.guard'
+import { StatusModule } from './status/status.module'
+import { RolesModule } from './roles/roles.module'
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, StatusModule, RolesModule],
   controllers: [],
   providers: [
     {
