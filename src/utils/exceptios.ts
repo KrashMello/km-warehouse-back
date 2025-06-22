c59bc58f6da67cdde166db6e862840214eebdd9b
KrashMello <joelenrikesoteldo@gmail.com>
2025-06-22 18:23:34 -0400

    feat: update exception utils
    se actualizo el type de las response

--- src/utils/exceptios.ts
+++ src/utils/exceptios.ts
@@ -1,8 +1,55 @@
 import { HttpException, HttpStatus } from '@nestjs/common'
+
 export interface opt {
   data: string | Record<string, any>
   type?: 'WARNING' | 'SUCCESS' | 'DANGER' | 'INFO'
-  status: HttpStatus
+  status: keyof typeof response
+}
+
+export type ResponseService = Promise<{
+  data: Record<string, any>
+  type?: 'WARNING' | 'SUCCESS' | 'DANGER' | 'INFO'
+  status: keyof typeof response
+}>
+
+export type partialOpt = Pick<opt, 'data' | 'type'>
+
+const response = {
+  404: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.NOT_FOUND
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
+  400: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.BAD_REQUEST
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
+  201: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.CREATED
+    throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
+  },
+  200: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.OK
+    throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
+  },
+  401: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.UNAUTHORIZED
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
+  403: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.FORBIDDEN
+    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
+  },
+  409: (opt: partialOpt) => {
+    const { data, type } = opt
+    const status = HttpStatus.CONFLICT
+    throw new HttpException({ type: type ?? 'DANGER', data, status }, status)
+  },
 }
 export default (opt: opt) => {
   const { status, data, type } = opt
@@ -12,41 +59,4 @@ export default (opt: opt) => {
 export const HttpResponse = (opt: opt) => {
   const { status, data, type } = opt
   return response[status]({ data, type })
-}
-const response = {
-  404: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.NOT_FOUND
-    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
-  },
-  400: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.BAD_REQUEST
-    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
-  },
-  201: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.CREATED
-    throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
-  },
-  200: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.OK
-    throw new HttpException({ type: type ?? 'SUCCESS', data, status }, status)
-  },
-  401: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.UNAUTHORIZED
-    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
-  },
-  403: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.FORBIDDEN
-    throw new HttpException({ type: type ?? 'WARNING', data, status }, status)
-  },
-  409: (opt: opt) => {
-    const { data, type } = opt
-    const status = HttpStatus.CONFLICT
-    throw new HttpException({ type: type ?? 'DANGER', data, status }, status)
-  },
 }
