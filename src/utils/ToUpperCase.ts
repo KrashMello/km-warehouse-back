efa03f3d10b157e729fc1f67cf699e03bebf0e90
KrashMello <joelenrikesoteldo@gmail.com>
2025-06-26 14:09:10 -0400

    feat: add toUpperCase decorator

---
+++ src/utils/ToUpperCase.ts
@@ -0,0 +1,6 @@
+import { Transform } from 'class-transformer'
+export function ToUpperCase() {
+  return Transform(({ value }) =>
+    typeof value === 'string' ? value.toUpperCase() : value,
+  )
+}
